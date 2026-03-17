2c65a14298fc16bbd0274c316b5bdfd699d2391b
keonrok-create <keonrok@gmail.com>
2026-03-17 23:28:46 +0900

    Refactor setAll method to use destructuring

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -12,10 +11,10 @@ export function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet: any) {
+        setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach((cookie: any) =>
-              cookieStore.set(cookie.name, cookie.value, cookie.options)
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
             );
           } catch {}
         },
